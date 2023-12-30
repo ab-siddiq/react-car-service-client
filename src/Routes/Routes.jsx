e367fdec0932ce8e2560e0f60ae27c374983505f
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2023-12-30 15:27:12 +0600

    fix the route error

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -19,11 +19,11 @@ const router = createBrowserRouter([
         path: '/login',
         element: <Login></Login>
       },
+      {
+        path: '/checkout/:id',
+        element: <CheckOut></CheckOut>
+      },
     ]
-    {
-      path: '/checkout/:id',
-      element: <CheckOut></CheckOut>
-    },
   },
 ]);
 
