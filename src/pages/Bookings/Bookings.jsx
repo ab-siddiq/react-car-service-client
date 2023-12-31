f87f6ea7be80ba223b9cf61af9ddfed4c72c6967
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2023-12-31 18:15:25 +0600

    implement delete booking data

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -10,12 +10,48 @@ const Bookings = () => {
       .then((res) => res.json())
       .then((data) => setBookings(data));
   }, []);
+  const handleDeleteBooking = (id) => {
+    const proceed = confirm("Are you sure to delete??");
+    if (proceed) {
+      console.log(id);
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if(data.deletedCount>0){
+            alert('deleted')
+            const remaining = bookings.filter(booking=>booking._id!==id)
+            setBookings(remaining)
+          }
+        });
+    }
+  };
   return (
     <div className="overflow-x-auto mx-auto h-screen mt-24">
       <table className="table">
         {/* head */}
         <thead>
           <tr>
+            <th>
+              <button className="btn btn-circle btn-sm ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </th>
             <th>SL</th>
             <th>Image</th>
             <th>CustomerName</th>
@@ -29,18 +65,41 @@ const Bookings = () => {
         </thead>
         <tbody>
           {bookings.map((booking, ind) => (
-            <tr>
+            <tr key={booking._id}>
+              <td>
+                <button
+                  onClick={() => {
+                    handleDeleteBooking(booking._id);
+                  }}
+                  className="btn btn-circle btn-sm bg-red-500"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </td>
               <td>{ind + 1}</td>
               <td>
                 <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
+                  <div className="mask mask-squircle w-24 h-24">
                     <img src={booking.img} alt={booking.serviceName} />
                   </div>
                 </div>
               </td>
               <td>{booking.customerName}</td>
               <td>{booking.phone}</td>
-              <td>{booking.price}</td>
+              <td>${booking.price}</td>
               <td>{booking.serviceName}</td>
               <td>{booking.serviceId}</td>
               <td>{booking.email}</td>
